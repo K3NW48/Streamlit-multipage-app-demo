6c796989c62539a2062cbf50a9375b93ee312715
K3NW48 <noThanks>
2022-11-14 17:32:44 -0500

    web apps for Machine Learning

---
+++ Aaniin.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Aaniin",
+    page_icon=":wave:",
+)
+
+st.write("# Boozhoo and Welcome to Streamlit! :wave:")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    This app was written with Vim and followed Streamlit's
+    multipage app tutorial!
+
+    Streamlit is pretty cool and I can see myself
+    making a few things in the ***near*** future!
+    
+    Check the sidebar for some other things!
+
+    Don't forget about all the goodies under my [linktr.ee/nbiish](https://linktr.ee/nbiish)
+
+    I'll be learning more machine learning things and 
+    trying to apply them to First Nations issues! ^,^
+
+    """
+)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Streamlit-multipage-app-demo
+# Streamlit-multipage-app-demo  
+  
+
 Written with Vim. I followed through the process of making an app and learning Streamlit vs copy pasting code.
